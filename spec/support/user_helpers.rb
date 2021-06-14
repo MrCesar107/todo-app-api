503db9a6fe13a6260781669c5bb4581c2800473c
Cesar Rodriguez <cesar.rodriguez.lara54@gmail.com>
2021-06-13 23:49:16 -0500

    Config rspec for unit tests

---
+++ spec/support/user_helpers.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require 'faker'
+require 'factory_bot_rails'
+
+module UserHelpers
+  def create_user
+    FactoryBot.create(:user,
+                      name: FFaker::Name.name,
+                      email: FFaker::Internet.email,
+                      username: FFaker::Internet.user_name,
+                      password: FFaker::Internet.password)
+  end
+
+  def build_user
+    FactoryBot.build(:user,
+                     name: FFaker::Name.name,
+                     email: FFaker::Internet.email,
+                     username: FFaker::Internet.user_name,
+                     password: FFaker::Internet.password)
+  end
+end
