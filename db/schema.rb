189bd882d8785fc126e640972d3d5615b6e09970
Cesar Rodriguez <cesar.rodriguez.lara54@gmail.com>
2021-06-20 03:06:53 -0500

    Create workspaces for users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_13_043753) do
+ActiveRecord::Schema.define(version: 2021_06_20_074434) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,4 +37,13 @@ ActiveRecord::Schema.define(version: 2021_06_13_043753) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  create_table "workspaces", force: :cascade do |t|
+    t.string "name", null: false
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_workspaces_on_user_id"
+  end
+
+  add_foreign_key "workspaces", "users"
 end
