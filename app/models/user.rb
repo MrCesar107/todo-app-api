d9dfcd11e0688d04a07dd69cd4589395c44af7a3
Cesar Rodriguez <cesar.rodriguez.lara54@gmail.com>
2021-06-28 14:24:37 -0500

    Add validations to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
 
+  validates :email, :username, uniqueness: true
+  validates :name, :username, :email, :password, presence: true
+  validates_confirmation_of :password
+
   has_many :workspaces, dependent: :restrict_with_exception
 end
